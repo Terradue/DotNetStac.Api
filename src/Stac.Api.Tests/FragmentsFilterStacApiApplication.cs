705d478d8398736c860db4fdec2c85d1ae356c16
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2022-08-12 14:46:17 +0200

    fix

--- src/Stac.Api.Tests/FragmentsFilterStacApiApplication.cs
+++ src/Stac.Api.Tests/FragmentsFilterStacApiApplication.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Stac.Api.WebApi.Controllers.Fragments.Filter;
+using Stac.Api.WebApi.Implementations;
 
 namespace Stac.Api.Tests
 {
